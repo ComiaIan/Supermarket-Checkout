3754238b1e4208a1f87139c02f4c8d11225d39d3
ComiaIan <158026917+ComiaIan@users.noreply.github.com>
2024-07-18 20:16:03 +0800

    Update SupermarketCheckout.cpp

--- SupermarketCheckout.cpp
+++ SupermarketCheckout.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cctype>
 
+using namespace std;
+
+const int MAX_CART_ITEMS = 50;
+const int MAX_USERS = 20;
+
+typedef struct Product {
+  int id;
+}
